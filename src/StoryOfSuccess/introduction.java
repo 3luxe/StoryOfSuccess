6c4314ede94e148f50bb5772cbbec7d9d6b16c1e
Nikita <deluxegame91@gmail.com>
2021-07-21 12:45:43 +0300

    first commit

---
+++ src/StoryOfSuccess/introduction.java
@@ -0,0 +1,10 @@
+package StoryOfSuccess;
+
+public class introduction {
+    public static void main(String[] args) {
+        String str = ("Hello World!");
+        String reapeated = str.repeat(10);
+
+        System.out.println(reapeated);
+    }
+}
