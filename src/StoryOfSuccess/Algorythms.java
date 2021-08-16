3b54e87cc9219f1e805cda88f5114419117c0375
Nikita <deluxegame91@gmail.com>
2021-08-16 15:37:14 +0300

    //new

---
+++ src/StoryOfSuccess/Algorythms.java
@@ -0,0 +1,21 @@
+package StoryOfSuccess;
+
+
+public class Algorythms {
+
+    String str1 = new String("algorythms");
+
+}
+
+class Recursion {
+    public static void main(String[] args) {
+        Algorythms algo = new Algorythms();
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(" " + algo.str1);
+            i++;
+        }
+    }
+}
+
+// to be continued
