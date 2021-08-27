06d2509fde4557f35793ae528ace54f439028a04
supryhan <suprigan@gmail.com>
2021-08-27 19:50:10 +0300

    - add unit tests support

---
+++ src/main/java/algo/ArraysConverter.java
@@ -0,0 +1,22 @@
+package algo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArraysConverter {
+    public List<Integer> convertToIntList(List<String> arr) {
+        return new LinkedList<>(); // Code here
+    }
+
+    public List<String> convertToStringList(List<Integer> arr) {
+        return new LinkedList<>(); // Code here
+    }
+
+    public int[] convertToIntList(String[] arr) {
+        return new int[0]; // Code here
+    }
+
+    public String[] convertToStringList(int[] arr) {
+        return new String[0]; // Code here
+    }
+}
